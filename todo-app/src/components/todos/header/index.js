391abc656585d8b2d27873934bc1d31b865facc8
Berk Karataş <berk-karatas@protonmail.com>
2022-08-24 19:57:50 +0300

    send todo in Header with form

--- todo-app/src/components/todos/header/index.js
+++ todo-app/src/components/todos/header/index.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-function Header({ addTodo }) {
+function Header({todos, addTodo }) {
+     const [form,setForm] = useState({todo:'',completed:false});
+
+     const onChangeInput = (e) => {
+          setForm({...form,[e.target.name]:e.target.value})
+     }
+     const clearForm = (e) => {
+          e.preventDefault();
+          setForm({todo:''})
+     }
+     const handleKeyDown = (e) => {
+          if (e.key === 'Enter') {
+               e.preventDefault();
+               if(form.todos === ''){
+                    alert('Enter a todo')
+                    return false;
+               }
+               addTodo([...todos,form]);
+               console.log(form);
+               clearForm(e);
+          }
+        }
 
   return (
     <div>
           <header class="header">
                <h1>todos</h1>
                <form>
-                    <input class="new-todo" placeholder="What needs to be done?" autoFocus />
+                    <input 
+                         className="new-todo" 
+                         placeholder="What needs to be done?" 
+                         name='todo' 
+                         value={form.todo} 
+                         autoFocus 
+                         onChange={onChangeInput} 
+                         onKeyDown={handleKeyDown} 
+                    />
                </form>
           </header>
     </div>
