ed06b332beb517efe88c22c467e452b7f0341737
Berk Karataş <berk-karatas@protonmail.com>
2022-08-25 12:18:03 +0300

    Native Fetching using

--- fetching/src/components/User.js
+++ fetching/src/components/User.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 export default function User() {
+     const [users, setUsers] = useState([])
+     const [loading, setLoading] = useState(true)
+     useEffect(() => {
+          fetch('https://jsonplaceholder.typicode.com/users')
+               .then(res => res.json())
+               .then(data => setUsers(data))
+               .catch(err => console.log(err))
+               .finally(() => setLoading(false))
+     }, [])
+
+     if (loading) {
+          return <div>Loading...</div>
+     }
+
   return (
-    <div>User</div>
-  )
+    <div>
+          <h1>Users</h1>
+          {users.map(((user) => 
+               <div key={user.id}>
+                    <h3>{user.name}</h3>
+                    <p>{user.email}</p>
+               </div>
+               ))}
+          
+    </div>
+  );
 }
